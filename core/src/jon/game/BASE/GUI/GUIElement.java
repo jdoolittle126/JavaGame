b998c16f15ebedff1b43e1f54f79126095217091
jdoolittle126 <jdoolittle126@gmail.com>
2017-09-18 11:08:31 -0400

    dyrthjolk,;

--- core/src/jon/game/BASE/GUI/GUIElement.java
+++ core/src/jon/game/BASE/GUI/GUIElement.java
@@ -1,9 +1,12 @@
 package jon.game.BASE.GUI;
 
+import com.badlogic.gdx.graphics.g2d.NinePatch;
+
 import jon.game.PHYSICS.Shape;
 
 public abstract class GUIElement {
 	public Shape hitbox;
+	public NinePatch x;
 	
 	
 
