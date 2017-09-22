df1802701179bf9d3a27fbd8e13c9a6671276fa4
jdoolittle126 <jdoolittle126@gmail.com>
2017-09-21 21:21:50 -0400

    gui stuff

--- core/src/jon/game/gui/BasicWindow.java
+++ core/src/jon/game/gui/BasicWindow.java
@@ -1,25 +1,41 @@
 package jon.game.gui;
 
-import com.badlogic.gdx.graphics.g2d.NinePatch;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
 
-public abstract class BasicWindow extends Actor {
-	Stage stage;
-	Rectangle size;
-	int z;
-	NinePatch border;
-	Texture background;
-	
-	public BasicWindow() {
-		
+public abstract class BasicWindow extends Window {
+
+	public BasicWindow(String title, Skin skin) {
+		super(title, skin);
+	}
+
+	public BasicWindow(String title, Skin skin, String styleName) {
+		super(title, skin, styleName);
+	}
+
+	public BasicWindow(String title, WindowStyle style) {
+		super(title, style);
+	}
+
+	@Override
+	public void draw(Batch batch, float parentAlpha) {
+		// TODO Auto-generated method stub
+		super.draw(batch, parentAlpha);
+	}
+
+	@Override
+	protected void drawStageBackground(Batch batch, float parentAlpha, float x, float y, float width, float height) {
+		// TODO Auto-generated method stub
+		super.drawStageBackground(batch, parentAlpha, x, y, width, height);
+	}
+
+	@Override
+	protected void drawBackground(Batch batch, float parentAlpha, float x, float y) {
+		// TODO Auto-generated method stub
+		super.drawBackground(batch, parentAlpha, x, y);
 	}
 	
-	public void render() {
-		
-	}
 	
 	
 }
