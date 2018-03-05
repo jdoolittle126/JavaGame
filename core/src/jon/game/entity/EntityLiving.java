762214f2fd52630cf497f8551cb13993f300c940
jdoolittle126 <jdoolittle126@gmail.com>
2018-03-05 01:14:43 -0500

    PANTS

--- core/src/jon/game/entity/EntityLiving.java
+++ core/src/jon/game/entity/EntityLiving.java
@@ -12,11 +12,34 @@ public abstract class EntityLiving extends EntityDynamic {
 	private float food_ticks_remaining, water_ticks_remaining;
 	private float current_health;
 	private float movement_modifier;
+	
+	private float hunger_need, thirst_need;
+	
 	private ArrayList<Effect> effects;
 	public LivingStatistics living_stats;
 	
 	public EntityLiving() {
 		super();
+	}
+	
+	
+	@Override
+	public void act(float delta) {
+		
+		super.act(delta);
+	}
+	
+	public void init(){
+		
+	}
+	
+	private void updateFood() {
+		food_ticks_remaining--;
+		hunger_need = living_stats.stat_hunger;
+	}
+	
+	public void addEffect(Effect effect) {
+		
 	}
 	
 	public void comsume(Consumable consumable){
